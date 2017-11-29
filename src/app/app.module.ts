60b5f5f8aadc4cfdeecf1a163d5204b6ee92de14
kkoal <k.koal@student.htw-berlin.de>
2017-11-29 22:35:17 +0100

    add ionic pro error monitoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, Injectable, Injector } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -7,24 +7,43 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Pro } from '@ionic/pro';
+
+const IonicPro = Pro.init('d247ab47', {
+    appVersion: '0.0.1'
+});
+
+@Injectable()
+export class ToxioErrorHandler implements ErrorHandler {
+    ionicErrorHandler: IonicErrorHandler;
+
+    constructor(injector: Injector) {
+        try {
+            this.ionicErrorHandler = injector.get(IonicErrorHandler);
+        } catch (e) {
+            // Unable to get the IonicErrorHandler provider, ensure
+            // IonicErrorHandler has been added to the providers list below
+        }
+    }
+
+    handleError(err: any): void {
+        IonicPro.monitoring.handleNewError(err);
+        // Remove this if you want to disable Ionic's auto exception handling
+        // in development mode.
+        this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+    }
+}
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [MyApp, HomePage],
+    imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+    bootstrap: [IonicApp],
+    entryComponents: [MyApp, HomePage],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        IonicErrorHandler,
+        { provide: ErrorHandler, useClass: ToxioErrorHandler }
+    ]
 })
 export class AppModule {}
