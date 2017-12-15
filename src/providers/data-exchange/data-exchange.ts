4c6e51efd6b8b32edb8483c8d90496ea22529826
kkoal <k.koal@student.htw-berlin.de>
2017-12-15 20:48:56 +0100

    Fix android build error due to incorrect relative path

--- src/providers/data-exchange/data-exchange.ts
+++ src/providers/data-exchange/data-exchange.ts
@@ -1,10 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { HazardousSubstance, Response } from '../../interfaces/interfaces';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { catchError, tap, map } from 'rxjs/operators';
 
 /*
   Generated class for the DataExchangeProvider provider.
@@ -20,23 +19,27 @@ export class DataExchangeProvider {
 
     getHazardousSubstances(): Observable<HazardousSubstance[]> {
         return this.http
-            .get<Response<HazardousSubstance[]>>(
-                '/assets/data/hazardous-substances.json'
-            )
-            .map(response => response.data.hazardousSubstances)
-            .catch(this.handleError);
+            .get<Response<HazardousSubstance[]>>('./assets/data/hazardous-substances.json')
+            .pipe(
+                tap(console.log),
+                map(response => response.data.hazardousSubstances),
+                catchError(this.handleError)
+            );
     }
 
-    private handleError(err: HttpErrorResponse) {
-        let errorMessage = '';
+    private handleError(err: HttpErrorResponse): ErrorObservable {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage: string;
         if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
             errorMessage = `An error occurred: ${err.error.message}`;
         } else {
-            errorMessage = `Server returned code: ${
-                err.status
-            }, error message is: ${err.message}`;
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
         }
-        console.error(errorMessage);
-        return Observable.throw(errorMessage);
+        console.error(err);
+        return new ErrorObservable(errorMessage);
     }
 }
