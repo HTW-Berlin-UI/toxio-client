4147cbaefac82a6100e20cadd2678683d1fe5c06
kkoal <k.koal@student.htw-berlin.de>
2017-12-15 08:09:58 +0100

    Fix native event handling on android

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { APP_CONFIG, AppConfig } from '../../app/app.config';
+import { UnitOfWork } from '../../providers/providers';
 
 import { SELECT_HAZARDOUS_SUBSTANCE_PAGE } from '../pages.constants';
 
@@ -19,8 +20,13 @@ import { SELECT_HAZARDOUS_SUBSTANCE_PAGE } from '../pages.constants';
 export class WelcomePage {
     constructor(
         public navCtrl: NavController,
-        @Inject(APP_CONFIG) private appConfig: AppConfig
+        @Inject(APP_CONFIG) private appConfig: AppConfig,
+        private unitOfWork: UnitOfWork
     ) {}
+
+    ionViewDidLoad() {
+        this.unitOfWork.init();
+    }
 
     start(startAsManager: boolean = false): void {
         this.appConfig.currentUser.can.addUsage = startAsManager;
